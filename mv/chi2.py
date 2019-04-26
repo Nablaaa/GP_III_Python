f20848a86c5d1799f53086448bce0fae09757022
Henrik Wolf <Henrik.Wolf@mailbox.tu-dresden.de>
2019-04-26 17:09:58 +0200

    updated chi2 method to reflect the chi2 method described in the explanation

--- mv/chi2.py
+++ mv/chi2.py
@@ -7,12 +7,11 @@ import matplotlib.pyplot as plt
 
 
 
-def getchi(e,data):
+def getchi(e,data, error):
    chi=0
-   for i in range (len(data)):
-      n=int(data[i]/e+0.5)
-      
-      chi+=(n*e-data[i])**2
+   for i, j in zip(data, error):
+      n=int(i/e+0.5)
+      chi+=(n*e-i)**2 / (j**2)
    #print (chi)
    return chi
 
@@ -27,21 +26,28 @@ def main():
     # Erstelle einen Plotbereich
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    #ax.axis([10**-19, 2*10**-19, 0, 2*10**-35])       # Achsengrenzen
-    ax.set_xlabel("e")                          # Beschriftung x-Achse
+    #ax.set_xlim(1.8e-19, 2.1e-19)       # Achsengrenzen
+    #ax.set_ylim(0,2)
+    ax.set_xlabel("e [C]")                          # Beschriftung x-Achse
     ax.set_ylabel("chi2")                       # Beschriftung y-Achse
     ax.grid(linestyle='-', linewidth=1)
     N=200000
-    x=np.linspace(1.5*10**-19, 1.8*10**-19, N)
+    x=np.linspace(0.1*10**-19, 4*10**-19, N)
     y=np.zeros(N)
     #print (x)
-    data=[5.197*10**-18,3.0229*10**-18,7.674*10**-18,3.378*10**-18,5.689*10**-18,1.219*10**-18]
+    data=[7.157e-18,8.516e-18,6.755e-16,3.84e-19]
+    error = [1.696e-19, 1.722e-19, 5.345e-18, 6.518e-21]
     #data=[5.197*10**-18]
     for i in range(N):
-      y[i]=getchi(x[i],data)
+      y[i]=getchi(x[i],data, error)
       #print (y[i])
-    
+    data = np.asarray(data)
+    print(data/1.6e-19)
+    print(data/1.9e-19)
+    ax.set_title("Chi squared fit")
     ax.plot(x, y, marker='o',linestyle='', lw=1, markersize=1)
+    plt.axvline(1.91e-19, color="red")
+    plt.axhline(1, color = "orange")
     plt.show()
 
 
